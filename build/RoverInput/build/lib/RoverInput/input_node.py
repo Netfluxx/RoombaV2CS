441982f92ba1b3bdc351098e519451925847027b
Arno <arno.blan334@gmail.com>
2024-08-04 14:56:21 +0200

    added qos reliabilty best effort to input subscriber

--- build/RoverInput/build/lib/RoverInput/input_node.py
+++ build/RoverInput/build/lib/RoverInput/input_node.py
@@ -6,6 +6,10 @@ import curses
 class InputNode(Node):
     def __init__(self):
         super().__init__('input_node')
+        #reliability best effort qos profile for the subscriber (UDP-like)
+        self.qos_policy = rclpy.qos.QoSProfile(reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
+                                               history=rclpy.qos.HistoryPolicy.KEEP_LAST,
+                                               depth=1)
         self.publisher_ = self.create_publisher(String, 'rover_input', 10)
         # self.timer_period = 0.1
         # self.timer = self.create_timer(self.timer_period, self.timer_callback)
@@ -31,7 +35,7 @@ class InputNode(Node):
                 elif ch == ord('d'):
                     user_input = "right"
                 else:
-                    user_input = "sfesfestop"
+                    user_input = "stop"
                 msg = String()
                 msg.data = user_input
                 self.publisher_.publish(msg)
