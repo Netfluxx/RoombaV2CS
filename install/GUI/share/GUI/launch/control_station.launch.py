8d4be62d56838f1d12926685ec8dc751a5d009ba
Arno <arno.blan334@gmail.com>
2024-09-21 18:38:48 +0200

    changed joystick launch file params to not overwhelm the arduino master

--- install/GUI/share/GUI/launch/control_station.launch.py
+++ install/GUI/share/GUI/launch/control_station.launch.py
@@ -1,1 +1,48 @@
-/home/arno/RoombaV2CS/build/GUI/launch/control_station.launch.py
+#LAUNCH FILE FOR SLAM AND MANUAL CONTROL
+
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, Command
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.descriptions import ParameterValue
+
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    joy_params = os.path.join(get_package_share_directory('GUI'), 'config', 'joystick_params.yaml')
+
+    #launch the gui nodoe and the joystick launch file in the controller_input package
+    
+    gui_control_station = Node(
+        package='GUI',
+        executable='gui',
+        name='gui',
+        output='screen',
+        parameters=[]
+    )
+
+    #launch the joystick stuff
+    joy_node = Node(
+        package='joy',
+        executable='joy_node',
+        parameters=[joy_params]
+    )
+
+    teleop_node = Node(
+        package='teleop_twist_joy',
+        executable='teleop_node',
+        name='teleop_node',
+        parameters=[joy_params],
+        remappings=[('/cmd_vel', '/joystick_cmd_vel')]
+    )
+
+    return LaunchDescription([
+        joy_node,
+        teleop_node,
+        gui_control_station
+    ])
+
--- install/controller_input/share/controller_input/launch/joystick.launch.py
+++ install/controller_input/share/controller_input/launch/joystick.launch.py
@@ -1,1 +1,36 @@
-/home/arno/RoombaV2CS/build/controller_input/launch/joystick.launch.py
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from rclpy.qos import QoSProfile, QoSDurabilityPolicy, QoSHistoryPolicy, QoSReliabilityPolicy
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    joy_params = os.path.join(get_package_share_directory('controller_input'), 'config', 'joystick_params.yaml')
+
+    # Define the QoS profile with BEST EFFORT reliability
+    qos_profile = QoSProfile(
+        history=QoSHistoryPolicy.KEEP_LAST,
+        depth=10,  # Keep the last 10 messages
+        reliability=QoSReliabilityPolicy.BEST_EFFORT,  # Use BEST EFFORT reliability
+        durability=QoSDurabilityPolicy.VOLATILE
+    )
+
+    joy_node = Node(
+        package='joy',
+        executable='joy_node',
+        parameters=[joy_params]
+    )
+
+    teleop_node = Node(
+        package='teleop_twist_joy',
+        executable='teleop_node',
+        name='teleop_node',
+        parameters=[joy_params],
+        remappings=[('/cmd_vel', '/joystick_cmd_vel')]
+    )
+
+    return LaunchDescription([
+        joy_node,
+        teleop_node
+    ])
